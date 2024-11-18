9d7902641eb8b7b4f728de3f373a8eb5f2c92e2d
Gigiiase <gigiiaseshvili94@gmail.com>
2024-11-18 11:13:31 +0400

    mm

--- src/main.js
+++ src/main.js
@@ -1,1 +1,99 @@
 // Work in this directory: src
+
+// 1
+let firsname = gigiise;
+console.log(firsname);
+
+// 2 
+
+// difference 
+// "5" is a string 
+// and just 5 is a number
+// for example 
+// console.log("5" + 2); "52"
+// console.log(5 + 2);  7 
+
+// 3
+let age = 20;
+console.log(age);
+
+// 4
+
+let bornYear = 2004;
+let isAdult = 2000 > bornYear;
+
+console.log(isAdult);
+console.log(`$(bornYear == 2000)`);
+console.log('$(bornYear <= 2500)');
+
+// 5
+// bornYaer == 2000 is false
+// bornYear <= 2500 is true
+// 5 == 5 true
+// 5 < 7 true
+
+// 6
+let misalmeba = hello;
+console.log(misalmeba.toUpperCase())
+
+// 7
+let misalmeba2 = hello;
+console.log(misalmeba.toLoverCase())
+
+// 8
+// manamde teqst aris dabal registrshi da shemdeg magal registrshi
+
+// 9
+// stringi aris mnishvneloba
+
+// 10
+// the function is not working because the text is already lowercase
+
+// 11
+let age1 = prompt('How old are you?');
+let nextAge = number(age1) + 1;
+console.log(`Next year you will be ${nextAge}`);
+
+// 12
+let name = prompt('What is your name?');
+let agess = Number(prompt('How old are you?'));
+let isInSixties = agess >= 60;  // finish the code so that it prints true if the age is in 60s
+console.log(`${name} is in sixties: ${isInSixties}`);
+
+// 13
+// let x = 5;
+// console.log(x++);
+// console.log(x);
+// X = 6       
+
+// 14
+// let y = 5;
+// console.log(++y);
+// console.log(y);
+// y = 6 
+
+// 15
+// ertit meti da ertit naklebi
+
+// 16
+// lies in when the increment happens relative to the value being used.
+
+// 17
+// space
+
+// 18
+
+let a = 5;
+let b = '5';
+console.log(a == b);
+
+// 19
+
+let c = 10;
+let d = '12';
+console.log(c != d);
+
+// 20
+// ertmanets ewebeba nacvlad mimatebisa
+
+
